e64c8ee9a7dcb31d21bdf51ae927f3d69d5a2322
Matt Menzenski <matt@payitgov.com>
2023-03-31 16:04:44 -0500

    initial commit

---
+++ tap_mongodb/tap.py
@@ -0,0 +1,58 @@
+"""mongodb tap class."""
+
+from __future__ import annotations
+
+from singer_sdk import Tap
+from singer_sdk import typing as th  # JSON schema typing helpers
+
+# TODO: Import your custom stream types here:
+from tap_mongodb import streams
+
+
+class Tapmongodb(Tap):
+    """mongodb tap class."""
+
+    name = "tap-mongodb"
+
+    # TODO: Update this section with the actual config values you expect:
+    config_jsonschema = th.PropertiesList(
+        th.Property(
+            "auth_token",
+            th.StringType,
+            required=True,
+            secret=True,  # Flag config as protected.
+            description="The token to authenticate against the API service",
+        ),
+        th.Property(
+            "project_ids",
+            th.ArrayType(th.StringType),
+            required=True,
+            description="Project IDs to replicate",
+        ),
+        th.Property(
+            "start_date",
+            th.DateTimeType,
+            description="The earliest record date to sync",
+        ),
+        th.Property(
+            "api_url",
+            th.StringType,
+            default="https://api.mysample.com",
+            description="The url for the API service",
+        ),
+    ).to_dict()
+
+    def discover_streams(self) -> list[streams.mongodbStream]:
+        """Return a list of discovered streams.
+
+        Returns:
+            A list of discovered streams.
+        """
+        return [
+            streams.GroupsStream(self),
+            streams.UsersStream(self),
+        ]
+
+
+if __name__ == "__main__":
+    Tapmongodb.cli()
