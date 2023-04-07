6b26cc621d48101b56249ea6657d159d138f06be
Matt Menzenski <matt@payitgov.com>
2023-04-06 21:56:59 -0500

    try a test

--- tests/core_test.py
+++ tests/core_test.py
@@ -1,20 +1,42 @@
 """Tests standard tap features using the built-in SDK tests library."""
 
-import datetime
-
 from singer_sdk.testing import get_tap_test_class
 
 from tap_mongodb.tap import TapMongoDB
+from tap_mongodb.streams import CollectionStream
+from pymongo.mongo_client import MongoClient
+from pymongo.database import Database
 
-
-SAMPLE_CONFIG = {
-    "start_date": datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%d")
-    # TODO: Initialize minimal tap config
+included_database = {
+    "database": "test_service",
+    "collection": "TestDocument",
 }
 
+SAMPLE_CONFIG = {
+    "mongodb_connection_string": "mongodb://admin:password@localhost:27017/?replicaSet=rs0",
+    "database_includes:": [
+        included_database,
+    ],
+}
 
 # Run standard built-in tap tests from the SDK:
 TestTapMongoDB = get_tap_test_class(tap_class=TapMongoDB, config=SAMPLE_CONFIG)
 
 
-# TODO: Create additional tests as appropriate for your tap.
+def test_one_stream_is_discovered():
+    # given a collection in the database
+    with MongoClient(SAMPLE_CONFIG["mongodb_connection_string"]) as client:
+        database: Database = client.get_database(included_database["database"])
+        database.create_collection(included_database["collection"])
+
+        # when the tap's discover_streams method is invoked
+        tap: TapMongoDB = TapMongoDB(config=SAMPLE_CONFIG)
+        streams: list[CollectionStream] = tap.discover_streams()
+
+        # then a stream for that collection is returned
+        assert len(streams) == 1
+        stream: CollectionStream = streams[0]
+        assert (
+            stream.tap_stream_id
+            == f"{included_database['database']}_{included_database['collection']}"
+        )
