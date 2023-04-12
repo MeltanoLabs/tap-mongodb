ff0db44cb0a2508a11ef800788e78312927525b0
Matt Menzenski <matt@payitgov.com>
2023-04-12 12:33:06 -0500

    add support for prefix

--- tap_mongodb/tap.py
+++ tap_mongodb/tap.py
@@ -178,7 +178,8 @@ class TapMongoDB(Tap):
                 continue
 
             self.logger.info("Discovered collection %s.%s", db_name, collection)
-            stream_name = f"{db_name}_{collection}"
+            prefix = f"{self.config['prefix']}-" if self.config["prefix"] else ""
+            stream_name = f"{prefix}{db_name}_{collection}"
             entry = CatalogEntry.from_dict({"tap_stream_id": stream_name})
             entry.stream = stream_name
             schema = {
